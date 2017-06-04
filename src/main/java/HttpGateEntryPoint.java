e4d2d01e9331d0568b9cffb3fe050d75521bada0
andrew <Andrew.Kostousov@gmail.com>
2017-06-04 15:33:41 +0500

    v0

---
+++ src/main/java/HttpGateEntryPoint.java
@@ -0,0 +1,36 @@
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.rapidoid.net.Server;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class HttpGateEntryPoint {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+        props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+        props.put("schema.registry.url", "http://localhost:8881");
+        String schemaString = "{\"type\": \"record\", " +
+                "\"name\": \"kevent\"," +
+                "\"fields\": [" +
+                "{\"name\": \"timestamp\", \"type\": \"long\"}," +
+                "{\"name\": \"payload\", \"type\": \"bytes\"}" +
+                "]}";
+        Producer<String, GenericRecord> producer = new KafkaProducer<>(props);
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = parser.parse(schemaString);
+        Server server = new KHttpServer(schema, producer).listen(8888);
+        new BufferedReader(new InputStreamReader(System.in)).readLine();
+        server.shutdown();
+        producer.close();
+    }
+}
+
---
+++ src/main/java/KHttpServer.java
@@ -0,0 +1,78 @@
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.rapidoid.buffer.Buf;
+import org.rapidoid.http.AbstractHttpServer;
+import org.rapidoid.http.HttpStatus;
+import org.rapidoid.http.MediaType;
+import org.rapidoid.net.abstracts.Channel;
+import org.rapidoid.net.impl.RapidoidHelper;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class KHttpServer extends AbstractHttpServer {
+
+    private static final byte[] URI_HELLO = "/hello".getBytes();
+    private static final byte[] URI_NOOP = "/noop".getBytes();
+    private static final byte[] URI_GEN = "/gen".getBytes();
+    private static final byte[] URI_KLOAD_100 = "/kload100".getBytes();
+    private static final byte[] URI_KLOAD_1000 = "/kload1000".getBytes();
+    private static final byte[] HELLO_WORLD = "Hello, World!".getBytes();
+    private final Schema schema;
+    private final Producer<String, GenericRecord> producer;
+    private byte[] randomBytesSource;
+
+    public KHttpServer(Schema schema, Producer<String, GenericRecord> producer) {
+        this.schema = schema;
+        this.producer = producer;
+        Random random = new Random(UUID.randomUUID().hashCode());
+        this.randomBytesSource = new byte[Integer.MAX_VALUE - 5];
+        random.nextBytes(randomBytesSource);
+    }
+
+    @Override
+    protected HttpStatus handle(Channel ctx, Buf buf, RapidoidHelper req) {
+        if (req.isGet.value) {
+            boolean isKeepAlive = req.isKeepAlive.value;
+            if (matches(buf, req.path, URI_KLOAD_100)) {
+                return GenerateKafkaLoad(ctx, 100, isKeepAlive, true);
+            } else if (matches(buf, req.path, URI_KLOAD_1000)) {
+                return GenerateKafkaLoad(ctx, 1000, isKeepAlive, true);
+            } else if (matches(buf, req.path, URI_NOOP)) {
+                return ok(ctx, isKeepAlive, new byte[0], MediaType.APPLICATION_OCTET_STREAM);
+            } else if (matches(buf, req.path, URI_GEN)) {
+                return GenerateKafkaLoad(ctx, 1000, isKeepAlive, false);
+            } else if (matches(buf, req.path, URI_HELLO)) {
+                return ok(ctx, isKeepAlive, HELLO_WORLD, MediaType.TEXT_PLAIN);
+            }
+        }
+        return HttpStatus.NOT_FOUND;
+    }
+
+    private HttpStatus GenerateKafkaLoad(Channel ctx, int eventSize, boolean isKeepAlive, boolean publishToKafka) {
+        produceEvents(eventSize, publishToKafka);
+        return ok(ctx, isKeepAlive, new byte[0], MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    private void produceEvents(int eventSize, boolean publishToKafka) {
+        long timestamp = System.currentTimeMillis();
+        for (long nEvents = 0; nEvents < 100; nEvents++) {
+            GenericRecord kevent = new GenericData.Record(schema);
+            kevent.put("timestamp", timestamp);
+            kevent.put("payload", generatePayload(eventSize));
+            ProducerRecord<String, GenericRecord> data = new ProducerRecord<>("ktopic", kevent);
+            if (publishToKafka)
+                producer.send(data);
+        }
+    }
+
+    public byte[] generatePayload(int eventSize) {
+        int from = ThreadLocalRandom.current().nextInt(0, randomBytesSource.length - eventSize);
+        return Arrays.copyOfRange(randomBytesSource, from, from + eventSize);
+    }
+}
