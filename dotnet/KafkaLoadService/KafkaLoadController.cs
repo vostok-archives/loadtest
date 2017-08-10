7d828c49d18d2c8343b92746b1798e40b0cca4f7
johneg <johneg@skbkontur.ru>
2017-08-10 17:59:15 +0500

    Single instance kafkaProducer

--- dotnet/KafkaLoadService/KafkaLoadController.cs
+++ dotnet/KafkaLoadService/KafkaLoadController.cs
@@ -15,7 +15,7 @@ namespace KafkaService
         {
             try
             {
-                kafkaProducer = GetProducer();
+                kafkaProducer = KafkaProducerProvider.Get();
             }
             catch (Exception e)
             {
@@ -84,6 +84,28 @@ namespace KafkaService
         public void Error()
         {
             throw new Exception();
+        }
+    }
+
+    public static class KafkaProducerProvider
+    {
+        private static KafkaProducer kafkaProducer;
+
+        static KafkaProducerProvider()
+        {
+
+            var topology = TopologyService.GetTopology("Kafka");
+
+            var producerSetting = new KafkaSetting();
+            producerSetting.AddBootstrapServers(topology);
+            producerSetting.AddClientId("producer_1");
+
+            kafkaProducer = new KafkaProducer(producerSetting);
+        }
+
+        public static KafkaProducer Get()
+        {
+            return kafkaProducer;
         }
     }
 }
