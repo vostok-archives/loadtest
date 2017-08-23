2d69f70bb4355f7f1f4d6c8fd6e5ca654ccefd1f
johneg <johneg@skbkontur.ru>
2017-08-23 15:03:55 +0500

    Поправил consumer

--- dotnet/KafkaClient/KafkaConsumer.cs
+++ dotnet/KafkaClient/KafkaConsumer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Confluent.Kafka;
 using Confluent.Kafka.Serialization;
 
@@ -11,13 +12,23 @@ namespace KafkaClient
         public KafkaConsumer(KafkaSetting kafkaSetting, string topic, IDeserializer<T> deserializer, IObserver<T> observer)
         {
             var settings = kafkaSetting.ToDictionary();
-            consumer = new Consumer<Null, T>(settings, null, deserializer);
+            consumer = new Consumer<Null, T>(settings, new NullDeserializer(), deserializer);
             consumer.OnMessage += (s, e) => observer.OnNext(e.Value);
             consumer.OnError += (s, e) => observer.OnError(new Exception(e.Reason));
             consumer.OnConsumeError += (s, e) => observer.OnError(new Exception(e.Error.Reason));
+            consumer.OnPartitionsAssigned += (s, e) =>
+            {
+                consumer.Assign(e);
+            };
 
             consumer.Subscribe(topic);
-            consumer.Poll(100);
+            new Task(() =>
+            {
+                while (true)
+                {
+                    consumer.Poll(100);
+                }
+            }).Start();
         }
 
         public void Dispose()
