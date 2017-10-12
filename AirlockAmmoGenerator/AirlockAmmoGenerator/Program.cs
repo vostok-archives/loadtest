f4cb50a116856e9b1a7273c10bdcc77cd7721348
t_kirill <t_kirill@skbkontur.ru>
2017-10-12 23:18:39 +0500

    + Empty project for ammo generator

---
+++ AirlockAmmoGenerator/AirlockAmmoGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirlockAmmoGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
